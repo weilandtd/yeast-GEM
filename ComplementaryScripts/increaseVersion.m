2bf98f62c26221b71e83b62375923b373e932985
BenjaSanchez <bensan@chalmers.se>
2018-01-25 10:55:55 +0100

    feat: store .mat when releasing

--- ComplementaryScripts/increaseVersion.m
+++ ComplementaryScripts/increaseVersion.m
@@ -8,6 +8,10 @@ function increaseVersion(model,version)
 model.description = ['yeastGEM_v' version];
 saveYeastModel(model)
 
+%Store model as .mat (only for releases):
+mkdir('../ModelFiles/mat');
+save('../ModelFiles/mat/yeastGEM.mat','model');
+
 %Update version file:
 fid = fopen('../version.txt','wt');
 fprintf(fid,version);
