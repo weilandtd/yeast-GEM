0f2ebdf14096879aa74a5564893bf731b8bff145
hongzhonglu <luho@chalmers.se>
2018-03-11 14:45:47 +0100

    fix-met.prop: update metabolite annotation
    The metabolite annotation, including the detailed name, charge, keggID and chebiID was updated based on the latest chebi and kegg databases.
    
    When save the model, the errors occured:
    Undefined function or variable 'git'.
    Error in saveYeastModel (line 37)
    COBRAcommit = git('log -n 1 --format=%H');

---
+++ ComplementaryScripts/modelCuration/updateMetaboliteAnnotation.m
@@ -0,0 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% update the metabolite annoation information in the model
+% updateMetaboliteAnnotation.m is a function from cobra
+% March 11, 2018 by Hongzhong
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+model = readCbModel('yeastGEM.xml');
+update_annotation = load('updated metabolite annotation.mat')
+% update_annotation
+% column 1: Abbreviation
+% column 2: Description
+% column 3: Charged formula
+% column 4: Charge
+% column 5: Compartment
+% column 6: KEGG ID
+% column 7: ChEBI ID
+function model = updateMetaboliteAnnotation(model,update_annotation)
+for  i = 1 : size(model.mets, 1)
+model.metNames{1} = update_annotation.metabolite{i,2};
+model.metCharges(i,1) = str2double(update_annotation.metabolite{i,4});
+model.metKEGGID{i} = update_annotation.metabolite{i,6};
+model.metChEBIID{i} = update_annotation.metabolite{i,7};
+end
+
+%model.metCharges(isnan(model.metCharges))= []
+
+end
+
+
+
