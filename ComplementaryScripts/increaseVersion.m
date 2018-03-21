cf9433e4d3dce1e74afc06b80619f71a72a220c7
BenjaSanchez <bensan@chalmers.se>
2018-03-21 13:18:51 +0100

    feat: allow new formats in master

--- ComplementaryScripts/increaseVersion.m
+++ ComplementaryScripts/increaseVersion.m
@@ -5,7 +5,7 @@
 function increaseVersion(version)
 
 %Check if history has been updated:
-fid = fopen('../history.md','r');
+fid     = fopen('../history.md','r');
 history = fscanf(fid,'%s');
 fclose(fid);
 if ~contains(history,['yeast' version ':'])
@@ -19,6 +19,22 @@ model = readCbModel('../ModelFiles/xml/yeastGEM.xml');
 %Include tag in model:
 model.description = ['yeastGEM_v' version];
 saveYeastModel(model)
+
+%Allow .mat & .xls storage:
+copyfile('../.gitignore','backup')
+fin  = fopen('backup','r');
+fout = fopen('../.gitignore','w');
+still_reading = true;
+while still_reading
+  inline = fgets(fin);
+  if ~ischar(inline)
+      still_reading = false;
+  elseif ~startsWith(inline,'*.mat') && ~startsWith(inline,'*.xls')
+      fwrite(fout,inline);
+  end
+end
+fclose('all');
+delete('backup');
 
 %Store model as .mat (only for releases):
 save('../ModelFiles/mat/yeastGEM.mat','model');
