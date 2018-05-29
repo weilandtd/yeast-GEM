3e7dcd78e34b87a9cb955ecc78cae32f5c8c90f1
Benjamín Sánchez <bensan@chalmers.se>
2018-05-29 11:52:30 +0200

    fix: display

--- ComplementaryScripts/otherChanges/addSLIMErxns.m
+++ ComplementaryScripts/otherChanges/addSLIMErxns.m
@@ -69,10 +69,10 @@ cd otherChanges
 sol_1 = optimizeCbModel(model);
 mu_1  = sol_1.x(strcmp(model.rxnNames,'growth'));                       %1/h
 qs_1  = sol_1.x(strcmp(model.rxnNames,'D-glucose exchange'))/1000*180;  %g(glucose)/gDWh
-disp(['Growth yield for original model: ' num2str(-mu_1/qs_1) ' gDW/g(glucose)'])
+disp(['Biomass yield for original model: ' num2str(-mu_1/qs_1) ' gDW/g(glucose)'])
 sol_2 = optimizeCbModel(model_SLIMEr);
 mu_2  = sol_2.x(strcmp(model_SLIMEr.rxnNames,'growth'));                       %1/h
 qs_2  = sol_2.x(strcmp(model_SLIMEr.rxnNames,'D-glucose exchange'))/1000*180;  %g(glucose)/gDWh
-disp(['Growth yield for model with SLIME rxns: ' num2str(-mu_2/qs_2) ' gDW/g(glucose)'])
+disp(['Biomass yield for model with SLIME rxns: ' num2str(-mu_2/qs_2) ' gDW/g(glucose)'])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
