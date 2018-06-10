ab3a9cef8181cc6b8a726b0ed810cc2363f519e8
Benjamín Sánchez <bensan@chalmers.se>
2018-06-10 15:42:45 +0200

    feat: automatic update of README
    date + model size is now updated when using saveYeastModel.m

--- ComplementaryScripts/saveYeastModel.m
+++ ComplementaryScripts/saveYeastModel.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % saveYeastModel(model)
 % Saves model as a .xml, .txt and .yml file. Also updates complementary
-% files (boundaryMets.txt and dependencies.txt).
+% files (boundaryMets.txt, README.md and dependencies.txt).
 %
 % Benjamín J. Sánchez
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -30,6 +30,31 @@ for i = 1:length(model.rxns)
     end
 end
 fclose(fid);
+
+%Update README file: date + size of model
+copyfile('../README.md','backup.md')
+fin  = fopen('backup.md','r');
+fout = fopen('../README.md','w');
+still_reading = true;
+while still_reading
+    inline = fgets(fin);
+    if ~ischar(inline)
+        still_reading = false;
+    else
+        if startsWith(inline,'* Last update: ')
+            inline = ['* Last update: ' datestr(datetime,'yyyy-mm-dd') newline];
+        elseif startsWith(inline,'|_Saccharomyces cerevisiae_|')
+            inline = ['|_Saccharomyces cerevisiae_|[Yeast 7.6]' ...
+                '(https://sourceforge.net/projects/yeast/)|' ...
+                num2str(length(model.rxns)) '|' ...
+                num2str(length(model.mets)) '|' ...
+                num2str(length(model.genes)) '|' newline];
+        end
+        fwrite(fout,inline);
+    end
+end
+fclose('all');
+delete('backup.md');
 
 %Convert notation "e-005" to "e-05 " in stoich. coeffs. to avoid
 %inconsistencies between Windows and MAC:
