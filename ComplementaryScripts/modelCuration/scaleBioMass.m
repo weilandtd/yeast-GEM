c5b098a9b172a67ed429eb18d4bd54accd834bba
benjasanchez <bensan@chalmers.se>
2018-09-07 14:31:01 +0200

    fix-rxn.prop: updated biomass composition data
    rescaled some values with data from a recent study with reasonable lipid content (Lahtvee et al. 2017)

--- ComplementaryScripts/modelCuration/scaleBioMass.m
+++ ComplementaryScripts/modelCuration/scaleBioMass.m
@@ -3,6 +3,8 @@
 % Corrects the stoichiometry coefficients of all pseudo-rxns in an
 % iterative fashion:
 % 1. Switch back to original model's abundance values (Forster et al. 2003)
+% 2. Improve with data from a more recent study (Lahtvee et al. 2017)
+% compatible with the current 8% lipid fraction
 % 
 % Function adapted from SLIMEr: https://github.com/SysBioChalmers/SLIMEr
 %
@@ -47,12 +49,59 @@ for i = 1:length(data.mets)
     %Change stoichiometry:
     model.S(metPos,rxnPos) = sign(model.S(metPos,rxnPos))*data.abundances(i);
 end
+[~,P,~,R,~,~,~] = sumBioMass(model,data);
+
+%Correct with data from Lahtvee 2017:
+fid = fopen('../../ComplementaryData/physiology/biomassComposition_Lahtvee2017.tsv');
+Lahtvee2017      = textscan(fid,'%s %s %f32 %f32 %s','Delimiter','\t','HeaderLines',1);
+data2.mets       = Lahtvee2017{1};
+data2.names      = Lahtvee2017{2};
+data2.abundances = double(Lahtvee2017{3});
+fclose(fid);
+for i = 1:length(data2.mets)
+    metID     = data2.mets{i};
+    metName   = data2.names{i};
+    abundance = data2.abundances(i);
+    if strcmp(metName,'protein')      %protein fraction
+        fP    = abundance/P;        %ratio to scale
+        model = rescalePseudoReaction(model,'protein',fP);
+        
+    elseif strcmp(metName,'RNA')      %RNA fraction
+        fR    = abundance/R;        %ratio to scale
+        model = rescalePseudoReaction(model,'RNA',fR);
+        
+    else    %Some extra carbohydrates:
+        modelPos = strcmp(model.mets,metID);
+        compPos  = strcmp(data.mets,metID);
+        rxnPos   = strcmp(model.rxnNames,'carbohydrate pseudoreaction');
+        if model.S(modelPos,rxnPos) < 0
+            MW = data.MWs(compPos)-18;
+            model.S(modelPos,rxnPos) = -abundance/MW*1000;
+        end
+    end
+end
 sumBioMass(model,data);
 
 %Finally, save model:
 cd ..
 saveYeastModel(model)
 cd modelCuration
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function model = rescalePseudoReaction(model,metName,f)
+
+rxnName = [metName ' pseudoreaction'];
+rxnPos  = strcmp(model.rxnNames,rxnName);
+for i = 1:length(model.mets)
+    S_ir   = model.S(i,rxnPos);
+    isProd = strcmp(model.metNames{i},[metName ' [cytoplasm]']);
+    if S_ir ~= 0 && ~isProd
+        model.S(i,rxnPos) = f*S_ir;
+    end
+end
 
 end
 
