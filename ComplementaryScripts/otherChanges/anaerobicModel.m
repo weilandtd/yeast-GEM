a81a2cf0f934c6eae6638a6c7178081d15a0b96f
feiranl <feiranl@chalmers.se>
2020-05-28 01:27:10 +0200

    style: remove repetitive codes

--- ComplementaryScripts/otherChanges/anaerobicModel.m
+++ ComplementaryScripts/otherChanges/anaerobicModel.m
@@ -17,19 +17,13 @@ NGAM  = 0;      %Refit done in Jouthen et al. 2012
 model = changeGAM(model,GAM,NGAM);
 model = scaleBioMass(model,'protein',P,'carbohydrate');
 
-%2nd change: Removes the requirement of heme a in the biomass equation
+%2nd change: Removes the requirement of heme a, NAD(PH), coenzyme A in the biomass equation
 %            (not used under anaerobic conditions)
-mets = {'s_3714[c]','s_1198[c]','s_1203[c]','s_1207[c]','s_1212[c]'};
+mets = {'s_3714[c]','s_1198[c]','s_1203[c]','s_1207[c]','s_1212[c]','s_0529[c]'};
 [~,met_index] = ismember(mets,model.mets);
 model.S(met_index,strcmp(model.rxns,'r_4598')) = 0;
 
-%3rd change: Removes the requirement of coenzyme A in the biomass equation
-%            (not used under anaerobic conditions)
-mets = {'s_0529[c]'};
-[~,met_index] = ismember(mets,model.mets);
-model.S(met_index,strcmp(model.rxns,'r_4598')) = 0;
-
-%4th change: Changes media to anaerobic (no O2 uptake and allows sterol
+%3rd change: Changes media to anaerobic (no O2 uptake and allows sterol
 %            and fatty acid exchanges)
 model.lb(strcmp(model.rxns,'r_1992')) = 0;        %O2
 model.lb(strcmp(model.rxns,'r_1757')) = -1000;    %ergosterol
@@ -40,7 +34,7 @@ model.lb(strcmp(model.rxns,'r_2134')) = -1000;    %14-demethyllanosterol
 model.lb(strcmp(model.rxns,'r_2137')) = -1000;    %ergosta-5,7,22,24(28)-tetraen-3beta-ol
 model.lb(strcmp(model.rxns,'r_2189')) = -1000;    %oleate
 
-%5th change: Blocked pathways for proper glycerol production
+%4th change: Blocked pathways for proper glycerol production
 %Block oxaloacetate-malate shuttle (not present in anaerobic conditions)
 model.lb(strcmp(model.rxns,'r_0713')) = 0; %Mithocondria
 model.lb(strcmp(model.rxns,'r_0714')) = 0; %Cytoplasm
