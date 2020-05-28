8ccaa3eb7dead4f8b6f3b183f84408e0c4e15985
Benjamin Sanchez <bjsanchezb@gmail.com>
2020-05-28 11:35:14 +0200

    feat: BiGG compliance flag in read_yeast_model
    add a BiGG compliance flag that defaults to false when loading the model. If true, will read the annotation and use BiGG ids (if any)

--- ComplementaryScripts/io.py
+++ ComplementaryScripts/io.py
@@ -11,14 +11,58 @@ dotenv_path = find_dotenv()
 repo_path = dirname(dotenv_path)
 MODEL_PATH = f"{repo_path}/ModelFiles/xml/yeastGEM.xml"
 
-def read_yeast_model():
+def read_yeast_model(make_bigg_compliant=False):
     """Reads the SBML file of the yeast model using COBRA.
+
+    Parameters
+    ----------
+    make_bigg_compliant : bool, optional
+        Whether the model should be initialized with BiGG compliance or not.
+        If false, the original ids/names/compartments will be used instead.
 
     Returns
     -------
     cobra.core.Model
     """
+
+    # Load model:
     model = read_sbml_model(MODEL_PATH)
+
+    # Check if already BiGG compliant:
+    is_bigg_compliant = "x" in model.compartments
+
+    # Convert to BiGG compliant if not already:
+    if not is_bigg_compliant and make_bigg_compliant:
+        # Metabolite changes:
+        comp_dic = {"er":"r", "erm":"rm", "p":"x"}
+        for met in model.metabolites:
+            # Save original id in notes:
+            met.notes["Original ID"] = met.id
+            # Change name to not include compartment at the end:
+            met.name = met.name.replace(f" [{model.compartments[met.compartment]}]", "")
+            # Change compartment info:
+            if met.compartment in comp_dic:
+                met.compartment = comp_dic[met.compartment]
+            # Update id with BiGG information:
+            if "bigg.metabolite" in met.annotation:
+                met.id = f"{met.annotation['bigg.metabolite']}_{met.compartment}"
+            else:
+                met.id = met.id.replace(f"[{met.compartment}]", f"_{met.compartment}")
+
+        # Compartment changes:
+        comps = model.compartments
+        comps["r"] = "endoplasmic reticulum"
+        comps["rm"] = "endoplasmic reticulum membrane"
+        comps["x"] = "peroxisome"
+        model.compartments = comps
+
+        # Reaction changes:
+        for rxn in model.reactions:
+            # Update id with BiGG information:
+            if "bigg.reaction" in rxn.annotation:
+                rxn.notes["Original ID"] = rxn.id
+                rxn.id = rxn.annotation['bigg.reaction']
+
     return model
 
 def write_yeast_model(model):
